035679a8479ecf60661f922f69a925a456db45c6
Maximus Ponciano <maximus.poncianoo@gmail.com>
2026-06-20 20:46:34 -0300

    feat: add create question controller and validation rules

--- backend/src/routes/question.routes.js
+++ backend/src/routes/question.routes.js
@@ -1,6 +1,8 @@
 import express from "express";
 import ListQuestionController from "../app/http/controllers/QuestionApi/ListQuestionController.js";
 import GetQuestionController from "../app/http/controllers/QuestionApi/GetQuestionController.js";
+import CreateQuestionController from "../app/http/controllers/QuestionApi/CreateQuestionController.js";
+
 import authenticator from "../app/http/middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -9,5 +11,6 @@ router.use(authenticator);
 
 router.get("/", ListQuestionController);
 router.get("/:id", GetQuestionController);
+router.post("/", CreateQuestionController);
 
 export default router;

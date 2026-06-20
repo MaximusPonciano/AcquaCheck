abbe9217108ba3ce9cab0e9935cf2717e2b3056c
Maximus Ponciano <maximus.poncianoo@gmail.com>
2026-06-20 19:41:12 -0300

    feat: add create attraction controller and route

--- backend/src/routes/attraction.routes.js
+++ backend/src/routes/attraction.routes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import ListAttractionController from "../app/http/controllers/AttractionApi/ListAttractionController.js";
 import GetAttractionController from "../app/http/controllers/AttractionApi/GetAttractionController.js";
-
+import CreateAttractionController from "../app/http/controllers/AttractionApi/CreateAttractionController.js";
 import authenticator from "../app/http/middlewares/auth.middleware.js";
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.use(authenticator);
 
 router.get("/", ListAttractionController);
 router.get("/:id", GetAttractionController);
+router.post("/", CreateAttractionController);
 
 export default router;
